7620bc1cd7fee7b44b35029ab09001ecb15dc050
Bram Vanbilsen <bramvbilsen@gmail.com>
2020-03-20 11:26:35 +0100

    Started working on storing html

--- src/main/java/project1/labsessie/ClientS.java
+++ src/main/java/project1/labsessie/ClientS.java
@@ -6,6 +6,8 @@ import java.io.*;
 
 public class ClientS {
 
+    final String outputPath = "./output";
+
     public ClientS(int port) {
         this.port = port;
     }
@@ -16,6 +18,40 @@ public class ClientS {
         ClientS client = new ClientS(80);
         // client.head();
         client.get();
+    }
+
+    private boolean isChunkedBody(String headers) {
+        return headers.contains("Transfer-Encoding: chunked");
+    }
+
+    private void handleChunckedBody(BufferedReader bodyReader) throws IOException {
+        String body = "";
+        System.out.println("==========BODY==========");
+        // Chunk size ends with CRLF (\r\n). readLine handles this!
+        String chunkSizeStr = bodyReader.readLine();
+        System.out.println("Reading chunk of length: " + chunkSizeStr);
+        for (int chunkSize = Integer.parseInt(chunkSizeStr, 16); chunkSize > 0; chunkSize--) {
+            char nextBodyChar = (char) bodyReader.read();
+            System.out.print(nextBodyChar);
+            body += nextBodyChar;
+        }
+
+        String filePath = "test.html";
+        File myObj = new File(filePath);
+        if (myObj.createNewFile()) {
+            System.out.println("File created: " + myObj.getName());
+        } else {
+            System.out.println("File already exists.");
+        }
+        try {
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.write(body);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     public void head() {
@@ -66,10 +102,33 @@ public class ClientS {
                 writer.println();
 
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+
+                    String headers = "";
+
                     String line;
+                    System.out.println("==========HEADERS==========");
                     while ((line = reader.readLine()) != null) {
-                        System.out.println(line);
+                        headers += line;
+                        // HTTP request ends with a blank line. If the response has a body, it will be
+                        // after the blank line.
+                        if (line.equals("")) {
+                            System.out.println("=========================");
+                            System.out.println("=========================");
+                            System.out.println("=========================");
+                            break;
+                        }
                     }
+
+                    System.out.println("Chunked body: " + this.isChunkedBody(headers));
+                    this.handleChunckedBody(reader);
+
+                    // System.out.println("==========BODY==========");
+                    // while ((line = reader.readLine()) != null) {
+                    // System.out.println(line);
+                    // // HTTP request ends with a blank line. If the response has a body, it will
+                    // be
+                    // // after the blank line.
+                    // }
                 }
             }
             client.close();
